bd42681937680c281f04981f7837a24c208531f9
Baur Urazalinov <BurhanH@users.noreply.github.com>
2024-03-02 23:26:37 -0600

    Add files via upload

--- Producer/Producer/Program.cs
+++ Producer/Producer/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace Producer;
+﻿using System.Diagnostics.Metrics;
+using OpenTelemetry;
+using OpenTelemetry.Exporter.InfluxDB;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+
+namespace Producer;
 
 using System;
 using Confluent.Kafka;
@@ -7,6 +13,39 @@ class Program
 {
     public static void Main(string[] args)
     {
+        /*Action<ResourceBuilder> configureResource = r => r.AddService(
+            serviceName: "influx-exporter-test",
+            serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+            serviceInstanceId: Environment.MachineName);
+        
+        var meterProvider = Sdk.CreateMeterProviderBuilder()
+            .ConfigureResource(configureResource)
+            .AddInfluxDBMetricsExporter(options =>
+            {
+                options.Org = "communicatio";
+                options.Bucket = "communicatio-bucket";
+                options.Token = "DOCKER_INFLUXDB_INIT_ADMIN_TOKEN";
+                options.Endpoint = new Uri("http://influxdb:8086");
+                options.MetricsSchema = MetricsSchema.TelegrafPrometheusV2;
+            })
+            .AddMeter("Producer")
+            .Build();
+        */
+
+        Action<ResourceBuilder> configureResource = r => r.AddService(
+            serviceName: "Communicatio-Producer",
+            serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+            serviceInstanceId: Environment.MachineName);
+        
+        var meterProvider = Sdk.CreateMeterProviderBuilder()
+            .ConfigureResource(configureResource)
+            .AddConsoleExporter()
+            .AddMeter("Producer")
+            .Build();
+        
+        var meter = new Meter("Producer");
+        var pCounter = meter.CreateCounter<int>("producer.messages.sent");
+        
         var conf = new ProducerConfig { BootstrapServers = "redpanda:29092" }; // replace to localhost:9092 for local testing
 
         Action<DeliveryReport<Null, string>> handler = r =>
@@ -31,10 +70,14 @@ class Program
                 
                 p.Produce("test_topic", new Message<Null, string> { Value = DateTime.UtcNow.Microsecond.ToString() }, handler);
                 
+                pCounter.Add(1);
+                
                 Thread.Sleep(100);
             }
             // wait for up to 10 seconds for any inflight messages to be delivered.
             p.Flush(TimeSpan.FromSeconds(10));
         }
+
+        meterProvider.ForceFlush();
     }
 }
